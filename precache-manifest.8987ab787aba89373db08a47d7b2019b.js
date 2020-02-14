ca9c9efb2dd34b41ef7bc0ba3bcad3d34eb5935b
je66yliu <jerry@Jerrys-MacBook-Pro.local>
2020-02-14 00:21:38 -0600

    Updates

---
+++ precache-manifest.8987ab787aba89373db08a47d7b2019b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce37654ffc749a496040e0050f1929d6",
+    "url": "/blog-app/index.html"
+  },
+  {
+    "revision": "fe484e19b629aa136cd6",
+    "url": "/blog-app/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "72fe693fe29ef8706121",
+    "url": "/blog-app/static/css/main.d8ff57ef.chunk.css"
+  },
+  {
+    "revision": "fe484e19b629aa136cd6",
+    "url": "/blog-app/static/js/2.1abc4243.chunk.js"
+  },
+  {
+    "revision": "e3b5b219f0e44e0e11ee9134cfeee7c3",
+    "url": "/blog-app/static/js/2.1abc4243.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72fe693fe29ef8706121",
+    "url": "/blog-app/static/js/main.f16e52b7.chunk.js"
+  },
+  {
+    "revision": "256bad1b8bcb82373df1",
+    "url": "/blog-app/static/js/runtime-main.a015dc90.js"
+  }
+]);
