dba4e2763444c851790b65831fccd070ff0e146a
je66yliu <jerry@Jerrys-MacBook-Pro.local>
2020-02-13 23:55:34 -0600

    merging features

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,21 +2,26 @@ import React from 'react';
 import { auth } from '../firebase/Firebase';
 import { signInWithGoogle } from '../firebase/Firebase';
 
-const Header = ({ user }) => {
-    return (
-        <nav className="flex justify-end bb b--white-10 bg-dark-gray">
-            <div className="flex-grow pa3 flex items-center">
-                <a className="f6 link dib white dim mr3 mr4-ns" href="#0">
-                    {
-                        user ?
-                        <div onClick={() => auth.signOut()}>Sign Out</div>
-                        : <div onClick={signInWithGoogle}>Sign In</div>
-                    }
+import Tooltip from '@material-ui/core/Tooltip';
+
+const Header = ({ user }) => (
+    <nav className="flex justify-end bb b--white-10 bg-dark-gray">
+        <div className="flex-grow pa3 flex items-center">
+            <a className="f6 link dib white dim mr3 mr4-ns" href="#0">
+                {
+                    user ?
+                    <div onClick={() => auth.signOut()}>Sign Out</div>
+                    : <div onClick={signInWithGoogle}>Sign In</div>
+                }
+            </a>
+
+            <Tooltip arrow title={<span style={{ fontSize: '12px' }}>Create a Google Account</span>} aria-label="signup">
+                <a className="f6 dib white bg-animate hover-bg-white hover-black no-underline pv2 ph4 br-pill ba b--white-20" href="https://accounts.google.com/signup/v2/webcreateaccount?continue=https%3A%2F%2Faccounts.google.com%2FManageAccount&dsh=S470069364%3A1581635333339519&gmb=exp&biz=false&flowName=GlifWebSignIn&flowEntry=SignUp" target="_blank" rel="noopener noreferrer">
+                    Sign Up
                 </a>
-                <a className="f6 dib white bg-animate hover-bg-white hover-black no-underline pv2 ph4 br-pill ba b--white-20" href="#0">Sign Up</a>
-            </div>
-        </nav>
-    );
-};
+            </Tooltip>
+        </div>
+    </nav>
+);
 
 export default Header;
