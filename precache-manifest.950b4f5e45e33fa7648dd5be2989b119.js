019c5868f42e7d4edaea445c2b0034f59d243ea7
je66yliu <jerry@Jerrys-MacBook-Pro.local>
2020-02-15 00:08:27 -0600

    Updates

--- precache-manifest.950b4f5e45e33fa7648dd5be2989b119.js
+++ precache-manifest.950b4f5e45e33fa7648dd5be2989b119.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c796885e4e68542a48f8dd5360cd3b53",
+    "revision": "46fe5a9d3c4fda3fea71d09c691f323d",
     "url": "/blog-app/index.html"
   },
   {
-    "revision": "976d3769447628e49675",
+    "revision": "fd097aa6400a24b48b6a",
     "url": "/blog-app/static/css/2.eb45ad44.chunk.css"
   },
   {
-    "revision": "f88bfde51f36aa759842",
+    "revision": "410d39c15bca9afd5072",
     "url": "/blog-app/static/css/main.d1b05096.chunk.css"
   },
   {
-    "revision": "976d3769447628e49675",
-    "url": "/blog-app/static/js/2.e71ee7e2.chunk.js"
+    "revision": "fd097aa6400a24b48b6a",
+    "url": "/blog-app/static/js/2.7c55c723.chunk.js"
   },
   {
     "revision": "e3b5b219f0e44e0e11ee9134cfeee7c3",
-    "url": "/blog-app/static/js/2.e71ee7e2.chunk.js.LICENSE.txt"
+    "url": "/blog-app/static/js/2.7c55c723.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "f88bfde51f36aa759842",
-    "url": "/blog-app/static/js/main.35afcfda.chunk.js"
+    "revision": "410d39c15bca9afd5072",
+    "url": "/blog-app/static/js/main.8e8fd6a8.chunk.js"
   },
   {
     "revision": "256bad1b8bcb82373df1",
